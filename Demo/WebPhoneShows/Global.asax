82eb255731afb97afcdf3b299ad6c34119472f38
ourworldcn <ourworld_cn@msn.com>
2017-11-05 03:10:37 +0800

    2

---
+++ Demo/WebPhoneShows/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebPhoneShows.MvcApplication" Language="C#" %>
---
+++ Demo/WebPhoneShows/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPhoneShows.Startup))]
+namespace WebPhoneShows
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
