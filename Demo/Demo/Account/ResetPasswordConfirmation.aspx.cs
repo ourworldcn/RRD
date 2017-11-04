ea5fc4edef5bbd282d78ff78566aab8dd0a4273a
ourworldcn <ourworld_cn@msn.com>
2017-11-05 03:12:35 +0800

    1

--- Demo/Demo/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace Demo.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- Demo/WebPhoneShows/Global.asax.cs
+++ Demo/WebPhoneShows/Global.asax.cs
@@ -11,7 +11,7 @@ namespace WebPhoneShows
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
-        {
+        { 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
